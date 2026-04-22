80bd9597d54797e363406339593d3be38dd24319
Adam Komarek <aduk0506@gmail.com>
2026-04-22 09:44:54 +0200

    Random numbers generator-4

--- sorting.py
+++ sorting.py
@@ -26,6 +26,8 @@ def selection_sort(values):
     return values
 
 def bubble_sort(values):
+    plt.ion()
+    plt.show()
     for j in range(len(values)):
 
         for n in range(len(values)-1):
@@ -33,15 +35,26 @@ def bubble_sort(values):
             if values[n] > values[n+1]:
                 values[n], values[n+1] = values[n+1], values[n]
 
+            index_highlight1 = n
+            index_highlight2 = n + 1
+            colors = ["steelblue"] * len(values)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+            plt.clf()
+            plt.bar(range(len(values)), values, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
 
-        print(values)
+        # print(values)
+    plt.ioff()
+    plt.show()
     return values
 
 
 
 
 if __name__ == "__main__":
-    values = random_numbers(10)  # 10 čísel v rozsahu 0–100
+    values = random_numbers(30)  # 10 čísel v rozsahu 0–100
     print(values)  # např. [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
 
     small = random_numbers(5, low=0, high=20)# 5 čísel v rozsahu 0–20
